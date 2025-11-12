c34258090e1c1ab438bad802e2d9376999455531
pedrolnj <pedrojesus@id.uff.br>
2025-11-12 10:06:53 -0300

    p

--- Analise_Pratica.R
+++ Analise_Pratica.R
@@ -2,9 +2,12 @@ library(readxl)
 library(tidyverse)
 base = read_excel("Base Casos.xlsx")
 
+#Boxplot Estatura
 box_estatura <- ggplot(base, aes(y = Estatura)) +
   geom_boxplot(outlier.shape = 21, fill = "lightgreen") +
   labs(title = "Box Plot Estatura",
        y = "Estatura (em metros)") +
   theme_minimal()
 print(box_estatura)
+
+
