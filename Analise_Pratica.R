5f43d2ed2a629dd7a213b767479d331717cc94bf
gbpcoutinho <gbpcoutinho@id.uff.br>
2025-11-12 10:22:18 -0300

    acrescentando histograma

--- Analise_Pratica.R
+++ Analise_Pratica.R
@@ -10,4 +10,9 @@ box_estatura <- ggplot(base, aes(y = Estatura)) +
   theme_minimal()
 print(box_estatura)
 
-
+# Histograma idade
+hist_idade <- ggplot(base, aes(x = Idade)) +
+  geom_histogram(binwidth = 5, fill = "blue", color = "black", alpha = 0.7) +
+  labs(title = "Distribuição da Idade", x = "Idade", y = "Frequência") +
+  theme_minimal()
+print(hist_idade)
